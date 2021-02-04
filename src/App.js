b01a0408b0fe1e61213e009622fa130709b6bd4b
Jamie <kirkwood.jamie@gmail.com>
2021-02-04 10:16:56 -0500

    basic field component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import BasicField from './Components/BasicField';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>Profile</h3>
+      <BasicField field="firstName"></BasicField>
+      <BasicField field="lastName"></BasicField>
+      <BasicField field="phone"></BasicField>
+      <BasicField field="email"></BasicField>
     </div>
   );
 }
---
+++ src/Components/BasicField.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export default function BasicField (props) {
+    return (
+        <div>
+            <p>{props.field}</p>
+        </div>
+    )
+}
