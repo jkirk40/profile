0a1f19e714f057ef51b8828c741ae7d2621c9fe0
Jamie <kirkwood.jamie@gmail.com>
2021-02-04 11:36:49 -0500

    documentation

--- src/Components/BasicField.js
+++ src/Components/BasicField.js
@@ -25,9 +25,16 @@ export default function BasicField (props) {
 
     const handleClick = async () => {
         setWarning('processing...')
+        // In a real application this would be our call to the API, e.g. /api/updateField
+        // We would need to verify field before submitting, both here and on the back end
+        // Valid email/phone, proper variable type, no SQL injection, etc.
+        // Skipped here for time
         await Backend().setField(props.field, input)
+        // If the backend returned a warning we would see it here and post a message for the user
+        // In this case that's not possible so we just remove the loading text.
         setWarning('')
 
+        // Make sure we display the updated info
         getField();
     } 
 
